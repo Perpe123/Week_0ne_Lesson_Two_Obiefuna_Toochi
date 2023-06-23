cdc003a4c07ca2edabd4224e6e8dab5294e6fe21
Perpe123 <obiefuna2017@gmail.com>
2023-06-23 11:08:26 +0100

    push my lesson 2 tasks

---
+++ tasks/task_seven.js
@@ -0,0 +1,28 @@
+const dayOfWeek = 'friday'
+
+switch (dayOfWeek) {
+  case 'sunday' :
+    console.log('sunday: first day of the week')
+    break
+  case 'monday' :
+    console.log('monday: second day of the week')
+    break
+  case 'tuesday' :
+    console.log('tuesday: third day of the week')
+    break
+  case 'wednesday' :
+    console.log('wednesday: fourth day of the week')
+    break
+  case 'thursday' :
+    console.log('thursday: fifth day of the week')
+    break
+  case 'friday' :
+    console.log('friday: sixth day of the week')
+    break
+  case 'saturday' :
+    console.log('saturday: seventh day of the week')
+    break
+  default :
+    console.log('invalid day of the week')
+    break
+}
---
+++ tasks/task_six.js
@@ -0,0 +1,7 @@
+const number = 11
+
+if (number >= 5 && number <= 10) {
+  console.log('value is in the specified range')
+} else {
+  console.log('value is outside the specified range') // @ returns "value is outside the specified range"
+}
